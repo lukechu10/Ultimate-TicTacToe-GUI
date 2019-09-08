757f9f541ecf7ecb98dde557264b667563b0e3fb
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-09-08 14:02:45 -0700

    Prevent user from reloading app with Ctrl+R

--- src/js/driver.js
+++ src/js/driver.js
@@ -1,3 +1,14 @@
+const hotkeys = require('hotkeys-js');
+
+// prevent reload
+hotkeys('f5', (event, handler) => {
+    // Prevent the default refresh event under WINDOWS system
+    event.preventDefault();
+});
+hotkeys('ctrl+r, command+r', (event, handler) => {
+    event.preventDefault();
+});
+
 $(() => {
     window.link = new LinkInstance({
         replaceHead: true,
