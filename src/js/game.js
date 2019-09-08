45db3f73d204d81b425f11c679e7a823eba34795
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2019-09-08 15:04:43 -0700

    Fixed debug dependency

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,5 @@
 ﻿$(() => {
-    //window.uTicTacToe = require('ultimate-tictactoe-ai');
-    window.uTicTacToe = require('../../../Node Addon API Integration/exports');
+    window.uTicTacToe = require('ultimate-tictactoe-ai');
+    //window.uTicTacToe = require('../../../Node Addon API Integration/exports');
     window.game = new uTicTacToe.uTicTacToe();
 });
